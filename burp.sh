b29d6c03e5d65fbe197927a7352a7fa1ca67834f
mapleHacks <64471798+mapleHacks@users.noreply.github.com>
2020-08-22 15:07:04 +0530

    Update burp.sh

--- burp.sh
+++ burp.sh
@@ -1,7 +1,5 @@
-pip3 install gdown
-mkdir ~/BurpSuite
-echo "/usr/lib/jvm/java-11-openjdk-amd64/bin/java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:/root/BurpSuite/BurpSuiteLoader_v2020.8.jar -noverify -jar /root/BurpSuite/burpsuite_pro_v2020.8.jar" > ~/BurpSuite/run.sh
-chmod +x /root/BurpSuite/*.jar && chmod +x /root/BurpSuite/*.sh
-echo "alias burpsuite='cd /root/BurpSuite/ && ./run.sh'" > /root/.bashrc
+echo "/usr/lib/jvm/java-11-openjdk-amd64/bin/java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:~/BurpSuite/BurpSuiteLoader_v2020.8.jar -noverify -jar ~/BurpSuite/burpsuite_pro_v2020.8.jar" > ~/BurpSuite/run.sh
+chmod +x ~/BurpSuite/*.jar && chmod +x ~/BurpSuite/*.sh
+echo "alias burpsuite='cd ~/BurpSuite/ && ./run.sh'" > ~/.bashrc
 sudo apt-get install -y at-spi-core
-printf "[Desktop Entry]\nVersion=1.0\nType=Application\nName=Simple Terminal\nComment=\nExec=st\nIcon=utilities-terminal\nPath=\nTerminal=false\nStartupNotify=false" > /root/Desktop/Simple\ Terminal.desktop
+printf "[Desktop Entry]\nVersion=1.0\nType=Application\nName=Simple Terminal\nComment=\nExec=st\nIcon=utilities-terminal\nPath=\nTerminal=false\nStartupNotify=false" > ~/Desktop/Simple\ Terminal.desktop
